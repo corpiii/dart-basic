9780851cd344b85d56ebb81f5494ebc42a02c949
corpi <lj845854@gmail.com>
2023-11-30 12:19:07 +0900

    refactor: lib/problem1.dart 의 위치를 lib/problem1/problem1.dart 로 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dart_basic/problem/problem1.dart';
+import 'package:dart_basic/problem/problem1/problem1.dart';
 import 'package:dart_basic/problem/problem2/problem2.dart';
 import 'package:dart_basic/problem/problem3/problem3.dart';
 
@@ -6,5 +6,4 @@ void main(List<String> args) {
   // problem1();
   // problem2();
   problem3();
-
 }
