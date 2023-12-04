027f720d80a60b38a31c545e2c842dee6a469841
corpi <lj845854@gmail.com>
2023-12-04 15:45:25 +0900

    feat: Person 인스턴스 생성 후 List 추가 및 출력

--- lib/231204/list_set_map/person.dart
+++ lib/231204/list_set_map/person.dart
@@ -3,6 +3,22 @@
 /// 2) Map - 10명 학생의 시험 점수
 /// 3) Map - 대한민국의 도시별 인구수 (순서 상관 없음)
 
+void personProblem() {
+  List<Person> personList = [];
+
+  var hong = Person(name: '홍길동');
+  var han = Person(name: '한석봉');
+
+  personList.add(hong);
+  personList.add(han);
+
+  var comment = "";
+  for (var person in personList) {
+    comment += '${person.name}\n';
+  }
+  print(comment);
+}
+
 class Person {
   String name;
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:dart_basic/231204/wand.dart';
-import 'package:dart_basic/231204/wizard.dart';
+import 'package:dart_basic/231204/list_set_map/person.dart';
 
 void main(List<String> args) {
-  var wand = Wand(name: "딱총나무", power: 0.5);
-  var potter = Wizard(name: "해리포터", hp: 100, mp: 0, wand: wand);
+  personProblem();
 }
