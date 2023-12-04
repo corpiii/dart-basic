3a4180d1fe5308609f4454238f991979992aa9af
corpi <lj845854@gmail.com>
2023-12-04 15:04:55 +0900

    feat: Wizard 생성 및 name setter 시 유효성 검사기능 추가
    Not Null and must be at least 3 letters

--- lib/231204/wizard.dart
+++ lib/231204/wizard.dart
@@ -6,15 +6,42 @@ class Wizard {
   String _name;
   int _hp;
   int _mp;
-  Wand? _wand;
+  Wand? wand;
+
+  String get name => _name;
+
+  set name(String value) {
+    validateName(value);
+  }
+
+  int get hp => _hp;
+
+  set hp(int hp) {
+    _hp = min(0, hp);
+  }
+
+  int get mp => _mp;
+
+  set mp(int value) {
+    _mp = value;
+  }
 
   Wizard({
     required String name,
     required int hp,
     required int mp,
-    required Wand? wand,
+    required this.wand,
   })  : _name = name,
         _hp = hp,
-        _mp = mp,
-        _wand = wand;
+        _mp = mp {
+    validateName(name);
+  }
+
+  void validateName(String name) {
+    if (name.length >= 3) {
+      _name = name;
+    } else {
+      throw Exception('Name must be at least 3 letters');
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:dart_basic/231130/cleric.dart';
+import 'package:dart_basic/231204/wand.dart';
+import 'package:dart_basic/231204/wizard.dart';
 
 void main(List<String> args) {
-  var cleric = Cleric("아서스", hp: 50, mp: 10);
+  var wand = Wand(name: "딱총나무", power: 10);
+  var potter = Wizard(name: "해리포터", hp: 100, mp: 10, wand: wand);
+  print(potter);
+  potter.name = "해리";
 }
