6934afdae401a82b1de775d14ecaa7f642511068
corpi <lj845854@gmail.com>
2023-12-05 14:10:07 +0900

    feat: Slime 클래스 생성 (Hero를 생성하지 않아서 오류 발생 중)

---
+++ lib/231205/slime.dart
@@ -0,0 +1,15 @@
+class Slime {
+  int _hp;
+  final String _suffix;
+
+  Slime({
+    required String name,
+  })  : _hp = 50,
+        _suffix = name;
+
+  void attack(Hero hero) {
+    print('슬라임$_suffix이/가 공격했다.');
+    print('10의 데미지');
+    hero.hp -= 10;
+  }
+}
