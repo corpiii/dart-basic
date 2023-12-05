b75f3924680442ef125dbe4bf3345937a81b435c
corpi <lj845854@gmail.com>
2023-12-05 15:09:58 +0900

    feat: Book, Computer 클래스 생성

---
+++ lib/231205/abstract_interface/book.dart
@@ -0,0 +1,14 @@
+class Book {
+  String name;
+  int price;
+  String color;
+  String isbn;
+
+  Book({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.isbn,
+  });
+}
+
---
+++ lib/231205/abstract_interface/computer.dart
@@ -0,0 +1,13 @@
+class Computer {
+  String name;
+  int price;
+  String color;
+  String makerName;
+
+  Computer({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.makerName,
+  });
+}
