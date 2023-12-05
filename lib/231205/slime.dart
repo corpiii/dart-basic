b716833acd50e60082bf5fce48c78c596758c6b5
corpi <lj845854@gmail.com>
2023-12-05 14:48:58 +0900

    refactor: name -> suffix 변수 명 수정

--- lib/231205/slime.dart
+++ lib/231205/slime.dart
@@ -5,13 +5,14 @@ class Slime {
   final String _suffix;
 
   Slime({
-    required String name,
+    required String suffix,
   })  : _hp = 50,
-        _suffix = name;
+        _suffix = suffix;
 
   void attack(Hero hero) {
+    hero.hp -= 10;
+
     print('슬라임$_suffix이/가 공격했다.');
     print('10의 데미지');
-    hero.hp -= 10;
   }
 }
