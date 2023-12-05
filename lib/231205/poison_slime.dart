5705320dcfe7e054bf650f1973896e5e9ed1cfce
corpi <lj845854@gmail.com>
2023-12-05 14:52:54 +0900

    feat: PoisonSlime 클래스 생성 및 attack 메서드 생성

---
+++ lib/231205/poison_slime.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+import 'package:dart_basic/231205/slime.dart';
+import 'hero.dart';
+
+class PoisonSlime extends Slime {
+  static const _maxPoisonAttackCount = 5;
+
+  int _attackCount;
+
+  int get attackCount => _attackCount;
+
+  set attackCount(int value) {
+    _attackCount = max(_attackCount, value);
+  }
+
+  PoisonSlime({
+    required super.suffix,
+  }) : _attackCount = _maxPoisonAttackCount;
+
+  @override
+  void attack(Hero hero) {
+    super.attack(hero);
+
+    if (attackCount != 0) {
+       var dividedByFive = hero.hp / 5;
+       var roundedDamage = dividedByFive.round().toInt();
+
+       hero.hp -= roundedDamage.toInt();
+       attackCount -= 1;
+
+       print('추가로, 독 포자를 살포했다!');
+       print('$roundedDamage포인트의 데미지');
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:dart_basic/231204/list_set_map/person.dart';
+import 'package:dart_basic/231205/hero.dart';
+import 'package:dart_basic/231205/poison_slime.dart';
 
 void main(List<String> args) {
-  personProblem();
+  var slime = PoisonSlime(suffix: 'A');
+  var hero = Hero(49);
+
+  slime.attack(hero);
 }
