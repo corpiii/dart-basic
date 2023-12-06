2860469a409ad225ecda4f3f0e733b38d15dada1
corpi <lj845854@gmail.com>
2023-12-06 23:01:59 +0900

    refactor: 부대 지정 최대 인원 매직리터럴

--- lib/231206/starcraft/util/control_group.dart
+++ lib/231206/starcraft/util/control_group.dart
@@ -1,6 +1,7 @@
 import 'package:dart_basic/231206/starcraft/interface/army_unionable.dart';
 
 class ControlGroup {
+  static const int _maxGroupCapacity = 12;
   static int _groupNumberTracker = 1;
   int _groupNumber;
   List<ArmyUnionable> _group;
@@ -12,7 +13,7 @@ class ControlGroup {
   }
 
   void add(ArmyUnionable unit) {
-    if (_group.length < 12) {
+    if (_group.length < _maxGroupCapacity) {
       _group.add(unit);
     }
   }
