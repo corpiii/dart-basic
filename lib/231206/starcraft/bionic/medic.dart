f157db3f88bdfc8c58b651ebcbc1aeca31552807
corpi <lj845854@gmail.com>
2023-12-06 15:41:07 +0900

    feat: Medic 클래스의 바이오닉 유닛 치료 및 SCV 치료 기능 추가

--- lib/231206/starcraft/bionic/medic.dart
+++ lib/231206/starcraft/bionic/medic.dart
@@ -1,5 +1,12 @@
+import '../mechanic/scv.dart';
 import 'interface/bionic.dart';
 
 class Medic extends Bionic {
+  void heal(Bionic bionic) {
+    // todo
+  }
 
+  void healSCV(SCV scv) {
+    // todo
+  }
 }
