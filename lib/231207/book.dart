b2f498e0e35cc223885f5a75d403a15c5a6c54c4
corpi <lj845854@gmail.com>
2023-12-07 18:35:03 +0900

    feat: Book 클래스 생성

---
+++ lib/231207/book.dart
@@ -0,0 +1,78 @@
+void controlInstance() {
+  var dayList = [ for (var i = 1; i <= 5; i++) DateTime(2023, 12, i)];
+  List<Book> bookList = [
+    for (var i = 0; i < dayList.length; i++) Book(
+        title: 'Book$i', publishDate: dayList[i], comment: 'comment$i')
+  ];
+
+  bookList.shuffle();
+  bookList.sort();
+  bookList.printAll();
+
+  var now = DateTime.now();
+  var book = Book(title: 'title', publishDate: now, comment: '0');
+  var book1 = Book(title: 'title', publishDate: now, comment: '1');
+
+  print(book == book1); // true
+
+  Set<Book> sett = {book, book1};
+  print(sett.length); // prints '1'
+
+  var deepCopiedBook = book1.copyWith();
+  print(identical(book1, deepCopiedBook)); // false
+}
+
+class Book implements Comparable<Book> {
+  String title;
+  DateTime publishDate = DateTime.now();
+  String comment;
+
+  Book({
+    required this.title,
+    required this.publishDate,
+    required this.comment,
+  });
+
+  Book copyWith({
+    String? title,
+    DateTime? publishDate,
+    String? comment,
+  }) {
+    return Book(
+      title: title ?? this.title,
+      publishDate: publishDate ?? this.publishDate,
+      comment: comment ?? this.comment,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || this.hashCode == other.hashCode;
+
+  @override
+  int get hashCode =>
+      Object.hash(title.hashCode, publishDate);
+
+  @override
+  String toString() {
+    return 'Book{title: $title, publishDate: $publishDate, comment: $comment}';
+  }
+
+  @override
+  int compareTo(Book other) {
+    return this.publishDate.compareTo(other.publishDate);
+  }
+}
+
+extension PrintableBook on List<Book> {
+  @override
+  void printAll() {
+    var comment = 'Information: \n';
+
+    for (var bookInfo in this) {
+      comment += '${bookInfo.toString()},\n';
+    }
+
+    print(comment);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,6 @@
-import 'package:dart_basic/231206/starcraft/util/control_group.dart';
-
-import '231206/starcraft/protoss/protoss_unit/zealot.dart';
-import '231206/starcraft/protoss/protoss_unit/dragun.dart';
-
-import '231206/starcraft/terran/terran_unit/bionic/marine.dart';
-import '231206/starcraft/terran/terran_unit/bionic/medic.dart';
-import '231206/starcraft/terran/terran_unit/mechanic/scv.dart';
-import '231206/starcraft/terran/terran_unit/mechanic/tank.dart';
+import 'package:dart_basic/231207/book.dart';
 
 void main(List<String> args) {
-  var scv = SCV(hp: 50);
-  var medic = Medic(hp: 50);
-  var marine = Marine(hp: 10);
-  var tank = Tank(hp: 150);
-  var zealot = Zealot(hp: 30, shield: 30);
-  var dragun = Dragun(hp: 40, shield: 40);
-  var group = ControlGroup();
-
-  print('질럿 회복 전: ${zealot.hp}');
-  medic.heal(zealot);
-  print('질럿 회복 후: ${zealot.hp}\n');
-
-  print('마린 회복 전: ${marine.hp}');
-  medic.heal(marine);
-  print('마린 회복 후: ${marine.hp}\n');
-
-  print('드라군 회복 전: ${dragun.hp}');
-  scv.repair(dragun);
-  print('드라군 회복 후: ${dragun.hp}\n');
-
-  group.add(medic);
-  group.add(marine);
-  group.add(zealot);
-  group.add(tank);
-  group.add(scv);
-
-  group.showAllUnit();
+  controlInstance();
+  num a = 1;
 }
