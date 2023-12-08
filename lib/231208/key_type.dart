38fc18c99da87c4e26a611a853aaadf138aace57
corpi <lj845854@gmail.com>
2023-12-08 18:11:38 +0900

    style: enum의 숫자 주석 표시

--- lib/231208/key_type.dart
+++ lib/231208/key_type.dart
@@ -1,12 +1,12 @@
 enum KeyType {
   padlock(limitCount: 1024),
-  button(limitCount: 10000),
-  dial(limitCount: 30000),
-  finger(limitCount: 100000);
+  button(limitCount: 10000), // 10,000
+  dial(limitCount: 30000), // 30,000
+  finger(limitCount: 100000); // 1,000,000
 
   final int limitCount;
 
   const KeyType({required this.limitCount});
-  
+
   static int limitCountBy(KeyType keyType) => keyType.limitCount;
 }
