ee4103f70c5f1b458ee01070efff1a855a909d8a
corpi <lj845854@gmail.com>
2023-12-13 22:33:53 +0900

    feat: Future 연습문제 구현

--- lib/231213/async.dart
+++ lib/231213/async.dart
@@ -1,9 +1,42 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
-void processFile() {
+void processFile() async {
+  // 1
   File csvFile = File('lib/231213/sample.csv');
   String allString = csvFile.readAsStringSync();
 
   allString = allString.replaceAll('한석봉', '김석봉');
   csvFile.writeAsStringSync(allString);
+
+  // 2
+  getMovieInfo()
+  .then((value) {
+    Map<String, dynamic> map = jsonDecode(value);
+    print(map['director']);
+  });
+
+  // 3
+  try {
+    String result = await timeoutFuture().timeout(Duration(seconds: 5));
+    print(result);
+  } on TimeoutException {
+    print('timeout');
+  }
+}
+
+Future<String> getMovieInfo() async {
+  await Future.delayed(Duration(seconds: 2));
+  final mockData = {
+    'title' : 'star Wars',
+    'director' : 'George Lucas',
+    'year' : 1977,
+  };
+  return jsonEncode(mockData);
+}
+
+Future<String> timeoutFuture() async {
+  await Future.delayed(Duration(seconds: 6));
+  return 'ok';
 }
