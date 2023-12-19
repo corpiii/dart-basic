5a1032e86bca87b057d9ed689f701391b16c346d
corpi <lj845854@gmail.com>
2023-12-19 14:29:23 +0900

    feat: BookBorrowManager 인터페이스 생성

---
+++ lib/231219/book_manager/borrow_manager/interface/book_borrow_manager.dart
@@ -0,0 +1,10 @@
+import '../../model/book.dart';
+import '../../model/user.dart';
+
+abstract interface class BorrowManager {
+  void borrowBook(User user, Book book);
+  void renewBook(User user, Book book);
+
+  void printAllBorrowedBook();
+  void printHistory(User user);
+}
