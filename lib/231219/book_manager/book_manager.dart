196a8c573cf762c342bacd866396a6be1c720a10
corpi <lj845854@gmail.com>
2023-12-19 14:58:51 +0900

    feat: user 내에 id 추가, 입력하지 않아도 자동으로 증가하도록 설정

--- lib/231219/book_manager/book_manager.dart
+++ lib/231219/book_manager/book_manager.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:dart_basic/231219/book_manager/borrow_manager/interface/book_borrow_manager.dart';
 import 'package:dart_basic/231219/book_manager/user_manager/interface/user_manager.dart';
 
+import 'model/user.dart';
+
 class BookManager {
   static const _initUserInput = -1;
 
@@ -44,11 +46,52 @@ class BookManager {
     }
   }
 
-  void _startUserManagerScene() {
-    
-  }
-
   void _startBorrowManagerScene() {
 
+  }
+}
+
+extension UserManagament on BookManager {
+  void _startUserManagerScene() {
+    int userInput = BookManager._initUserInput;
+
+    while (userInput != 0) {
+      print('0.뒤로   1.회원조회   2.회원등록   3.회원수정   4.회원삭제');
+      try {
+        var input = stdin.readLineSync();
+        userInput = int.parse(input ?? "");
+      } catch (e) {
+        print('숫자를 입력해주세요.');
+        continue;
+      }
+
+      switch (userInput) {
+        case 0:
+          print('이전화면으로 이동합니다.');
+          break;
+        case 1:
+          userManager.printAllUser();
+          break;
+        case 2:
+          // userManager.addUser(user);
+          break;
+        case 3:
+          print('종료합니다.');
+          break;
+        default:
+          print('잘못된 입력입니다.');
+          break;
+      }
+    }
+  }
+
+  User generateUser() {
+    print('이름을 입력해주세요');
+    var name = stdin.readLineSync();
+    var address = stdin.readLineSync();
+    var phoneNumber = stdin.readLineSync();
+    var phoneNumber = stdin.readLineSync();
+
+    return User(name: name, address: address, phoneNumber: phoneNumber, birthDay: birthDay)
   }
 }
--- lib/231219/book_manager/model/user.dart
+++ lib/231219/book_manager/model/user.dart
@@ -1,4 +1,6 @@
 class User {
+  static int _incresedId = 0;
+
   int _id;
   String _name;
   String _address;
@@ -6,14 +8,15 @@ class User {
   DateTime _birthDay;
 
   User({
-    required int id,
     required String name,
     required String address,
     required String phoneNumber,
     required DateTime birthDay,
-  })  : _id = id,
+  })  : _id = _incresedId,
         _name = name,
         _address = address,
         _phoneNumber = phoneNumber,
-        _birthDay = birthDay;
+        _birthDay = birthDay {
+    _incresedId += 1;
+  }
 }
